2ecdbcab5e8211c8ecf730f1a21fd043c4cd57c3
Roxana Ion <ionroxana95@yahoo.com>
2017-08-24 11:02:03 +0200

    Add project files.

---
+++ EchoClient/EchoServerFacade.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace EchoClient
+{
+    class EchoServerFacade
+    {
+        private NetworkStream netStream;
+        private int port;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private string serverName;
+        private Socket serverSocket;
+
+        private void Close()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Echo()
+        {
+
+        }
+
+        public EchoServerFacade()
+        {
+
+        }
+
+        public void EchoUpper()
+        {
+
+        }
+
+        private void Open()
+        {
+
+        }
+
+        private void ReceiveFromServer()
+        {
+
+        }
+
+        private void SendToServer()
+        {
+
+        }
+    }
+}
---
+++ EchoClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static bool programRunning = true;
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+            StreamReader sr = new StreamReader(client.GetStream());
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            sw.AutoFlush = true;
+
+
+            while (programRunning)
+            {
+
+                sw.WriteLine(Console.ReadLine());
+                string data = sr.ReadLine();
+                Console.WriteLine(data);
+
+            }
+        }
+    }
+}
